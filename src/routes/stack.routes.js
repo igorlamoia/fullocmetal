10009d0803f029e311ea0bb47fa1de1df7fd44b7
Igor Lamoia Queiroz <igorlamoia@hotmail.com>
2022-06-11 14:46:32 -0300

    finalizando splash screen

--- src/routes/stack.routes.js
+++ src/routes/stack.routes.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { Home } from '../screens/Home'
-import { Details } from '../screens/Details'
-import { RentDate } from '../screens/RentDate'
-import { RentDetailsScreen } from '../screens/RentDetails'
-import { RentComplete } from '../screens/RentComplete'
-import { UserRents } from '../screens/UserRents'
-import Login from '../screens/Login'
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Home } from '../screens/Home';
+import { Details } from '../screens/Details';
+import { RentDate } from '../screens/RentDate';
+import { RentDetailsScreen } from '../screens/RentDetails';
+import { RentComplete } from '../screens/RentComplete';
+import { UserRents } from '../screens/UserRents';
+import Login from '../screens/Login';
 
-const { Navigator, Screen } = createNativeStackNavigator()
+const { Navigator, Screen } = createNativeStackNavigator();
 
 export const StackRoutes = () => {
-  return (
-    <Navigator
-      screenOptions={{
-        headerShown: false
-      }}
-    >
-      <Screen name="Login" component={Login} />
-      <Screen name="Home" component={Home} />
-      <Screen name="Details" component={Details} />
-      <Screen name="RentDate" component={RentDate} />
-      <Screen name="RentDetailsScreen" component={RentDetailsScreen} />
-      <Screen name="RentComplete" component={RentComplete} />
-      <Screen name="UserRents" component={UserRents} />
-    </Navigator>
-  )
-}
+	return (
+		<Navigator
+			screenOptions={{
+				headerShown: false,
+			}}
+		>
+			<Screen name="Login" component={Login} />
+			<Screen name="Home" component={Home} />
+			<Screen name="Details" component={Details} />
+			<Screen name="RentDate" component={RentDate} />
+			<Screen name="RentDetailsScreen" component={RentDetailsScreen} />
+			<Screen name="RentComplete" component={RentComplete} />
+			<Screen name="UserRents" component={UserRents} />
+		</Navigator>
+	);
+};
---
+++ src/screens/Splash/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+	flex: 1;
+	background-color: #1b1b1f;
+	justify-content: center;
+	align-items: center;
+`;
